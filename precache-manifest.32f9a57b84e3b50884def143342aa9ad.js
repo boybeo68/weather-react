d6835b4053235a4ab403519f918fb73e04b2ee5a
Tung, Nguyen Thac <tung.nguyen.thac@ntq-solution.com.vn>
2021-12-08 11:52:47 +0700

    Updates

---
+++ precache-manifest.32f9a57b84e3b50884def143342aa9ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "548677ce380754b13c5468353286c1f1",
+    "url": "/weather-react/index.html"
+  },
+  {
+    "revision": "2ba34304181ac4298f23",
+    "url": "/weather-react/static/css/main.050b8e45.chunk.css"
+  },
+  {
+    "revision": "44d91217a2106cc453b8",
+    "url": "/weather-react/static/js/2.6fc8ad99.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather-react/static/js/2.6fc8ad99.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2ba34304181ac4298f23",
+    "url": "/weather-react/static/js/main.0f8cc392.chunk.js"
+  },
+  {
+    "revision": "f0345a52712c5a71aa45",
+    "url": "/weather-react/static/js/runtime-main.523c1278.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/weather-react/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/weather-react/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
